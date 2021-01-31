7f9ef482ff5e93298d38c15872573fbfa3b5e98f
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-01-31 13:58:59 -0500

    Kind of wonky list but getting results

--- aws-golang-rest-api-with-dynamodb/todos/list.go
+++ aws-golang-rest-api-with-dynamodb/todos/list.go
@@ -1,19 +1,55 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/dynamodb"
+
+    "fmt"
+	"os"
+	"encoding/json"
 )
 
-// Handler function Using AWS Lambda Proxy Request
-func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	//Generate message that want to be sent as body
-	message := fmt.Sprintf(" { \"Message\" : \"Hello Friend \" } ")
+// type Item struct {
+// 	Id       string  `json:"id,omitempty"`
+//     Title    string  `json:"title"`
+//     Details  string  `json:"details"`
+// }
 
-	//Returning response with AWS Lambda Proxy Response
-	return events.APIGatewayProxyResponse{Body: message, StatusCode: 200}, nil
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+        SharedConfigState: session.SharedConfigEnable,
+    }))
+
+	svc := dynamodb.New(sess)
+	
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		TableName:                 aws.String(os.Getenv("DYNAMODB_TABLE")),
+	}
+
+	// Make the DynamoDB Query API call
+    result, err := svc.Scan(params)
+
+    if err != nil {
+        fmt.Println("Query API call failed:")
+        fmt.Println((err.Error()))
+        os.Exit(1)
+    }
+
+	fmt.Println("result: ", result)
+
+	res, err := json.Marshal(result)
+	if err != nil {
+        fmt.Println("Got error marshalling result")
+        fmt.Println(err.Error())
+        return events.APIGatewayProxyResponse{Body: "Error getting items", StatusCode: 500}, nil
+    }
+
+	return events.APIGatewayProxyResponse{Body: string(res), StatusCode: 200}, nil
 }
 
 func main() {
