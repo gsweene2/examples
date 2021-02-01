592468a927f34a26311e7661ee67d717b1c3bb47
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2021-01-31 20:40:36 -0500

    Formatted

--- aws-golang-rest-api-with-dynamodb/todos/create.go
+++ aws-golang-rest-api-with-dynamodb/todos/create.go
@@ -4,32 +4,31 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/google/uuid"
 
-    "fmt"
-    "os"
 	"encoding/json"
-	"reflect"
+	"fmt"
+	"os"
 )
 
 type Item struct {
-	Id       string  `json:"id,omitempty"`
-    Title    string  `json:"title"`
-    Details  string  `json:"details"`
+	Id      string `json:"id,omitempty"`
+	Title   string `json:"title"`
+	Details string `json:"details"`
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	
+
 	// Creating session for client
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
-    // Create DynamoDB client
-    svc := dynamodb.New(sess)
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
 
 	itemUuid := uuid.New().String()
 
@@ -39,36 +38,36 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	itemStruct := Item{}
 	json.Unmarshal([]byte(itemString), &itemStruct)
 
-    item := Item{
-		Id: itemUuid,
-        Title:   itemStruct.Title,
-        Details:  itemStruct.Details,
-    }
+	item := Item{
+		Id:      itemUuid,
+		Title:   itemStruct.Title,
+		Details: itemStruct.Details,
+	}
 
-    av, err := dynamodbattribute.MarshalMap(item)
-    if err != nil {
-        fmt.Println("Error marshalling item: ", err.Error())
-        return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
-    }
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		fmt.Println("Error marshalling item: ", err.Error())
+		return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
+	}
 
-    tableName := os.Getenv("DYNAMODB_TABLE")
+	tableName := os.Getenv("DYNAMODB_TABLE")
 
 	fmt.Println("Putting item: %v", av)
 
-    input := &dynamodb.PutItemInput{
-        Item:      av,
-        TableName: aws.String(tableName),
-    }
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
 
 	// PutItem request
 	_, err = svc.PutItem(input)
-	
+
 	// Checking for errors, return error
-    if err != nil {
-        fmt.Println("Got error calling PutItem: ", err.Error())
-        return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
+	if err != nil {
+		fmt.Println("Got error calling PutItem: ", err.Error())
+		return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
 	}
-	
+
 	item_marshalled, err := json.Marshal(item)
 
 	fmt.Println("Returning item: ", string(item_marshalled))
--- aws-golang-rest-api-with-dynamodb/todos/update.go
+++ aws-golang-rest-api-with-dynamodb/todos/update.go
@@ -4,72 +4,72 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 
-    "fmt"
-	"os"
 	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Item struct {
-	Id       string  `json:"id,omitempty"`
-    Title    string  `json:"title"`
-    Details  string  `json:"details"`
+	Id      string `json:"id,omitempty"`
+	Title   string `json:"title"`
+	Details string `json:"details"`
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// Creating session for client
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
-        SharedConfigState: session.SharedConfigEnable,
-    }))
+		SharedConfigState: session.SharedConfigEnable,
+	}))
 
 	// Create DynamoDB client
 	svc := dynamodb.New(sess)
-	
+
 	pathParamId := request.PathParameters["id"]
 
 	itemString := request.Body
 	itemStruct := Item{}
 	json.Unmarshal([]byte(itemString), &itemStruct)
-	
+
 	info := Item{
-		Title: itemStruct.Title,
+		Title:   itemStruct.Title,
 		Details: itemStruct.Details,
-    }
+	}
 
 	fmt.Println("Updating title to: ", info.Title)
 	fmt.Println("Updating details to: ", info.Details)
 
-    // Prepare input for Update Item
-    input := &dynamodb.UpdateItemInput{
-        ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-            ":t": {
-                S: aws.String(info.Title),
+	// Prepare input for Update Item
+	input := &dynamodb.UpdateItemInput{
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":t": {
+				S: aws.String(info.Title),
 			},
 			":d": {
 				S: aws.String(info.Details),
 			},
-        },
-        TableName: aws.String(os.Getenv("DYNAMODB_TABLE")),
-        Key: map[string]*dynamodb.AttributeValue{
+		},
+		TableName: aws.String(os.Getenv("DYNAMODB_TABLE")),
+		Key: map[string]*dynamodb.AttributeValue{
 			"id": {
 				S: aws.String(pathParamId),
 			},
 		},
-        ReturnValues:              aws.String("UPDATED_NEW"),
-        UpdateExpression:          aws.String("set title = :t, details = :d"),
-    }
+		ReturnValues:     aws.String("UPDATED_NEW"),
+		UpdateExpression: aws.String("set title = :t, details = :d"),
+	}
 
 	// UpdateItem request
 	_, err := svc.UpdateItem(input)
-	
+
 	// Checking for errors, return error
-    if err != nil {
-        fmt.Println(err.Error())
-        return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
-    }
+	if err != nil {
+		fmt.Println(err.Error())
+		return events.APIGatewayProxyResponse{Body: "Yikes", StatusCode: 500}, nil
+	}
 
 	return events.APIGatewayProxyResponse{Body: string("Done"), StatusCode: 200}, nil
 }
